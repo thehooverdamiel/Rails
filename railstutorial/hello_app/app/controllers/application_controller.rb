13b39234ae28442c22fa7ad0c3e0a10947788335
Daniel <dgrimes@levelsetsolutions.com>
2017-12-13 16:53:52 -0500

    small tweaks for exercieses at end of sec 1.3

--- railstutorial/hello_app/app/controllers/application_controller.rb
+++ railstutorial/hello_app/app/controllers/application_controller.rb
@@ -2,7 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def hello
-		render html: "hello, world!"
+		render html: "¡Hola, mundo!"
 	end
 
+	def goodbye
+		render html: "Adios, mundo."
+	end
 end
